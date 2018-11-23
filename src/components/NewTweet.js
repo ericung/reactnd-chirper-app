7eebc28027384561db3506702f7040c3a79fa2ef
pietore <ungericw@tutanota.com>
2018-11-23 08:28:12 -0600

    new tweet functionality

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
   state = {
@@ -13,8 +15,10 @@ class NewTweet extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const { text } = this.state
+    const { dispatch, id } = this.props
     // todo: Add Tweet to Store
     console.log('New Tweet: ', text)
+    dispatch(handleAddTweet(text, id))
     this.setState(() => ({
       text: ''
     }))
@@ -54,4 +58,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet 
+export default connect()(NewTweet) 
